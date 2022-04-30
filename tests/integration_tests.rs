4c6a0e730e813f614c3cc106ad8f86f23157464a
Yogesh Dhamija <ydhamija96@gmail.com>
2022-04-29 22:15:45 -0500

    chore: tests read cleaner, runner helper takes single string args

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -4,39 +4,39 @@ use predicates::prelude::*;
 
 #[test]
 fn displays_help() {
-    let assert = run_with(&["-h"]);
+    let assert = run_with("-h");
     assert.success().stdout(predicate::str::contains("USAGE"));
 }
 
 #[test]
 fn displays_version() {
-    let assert = run_with(&["-V"]);
+    let assert = run_with("-V");
     assert.success();
 }
 
 #[test]
 fn fails_with_random_args() {
-    let assert = run_with(&["random"]);
+    let assert = run_with("random");
     assert.failure();
 }
 
 #[test]
 fn starts_program() {
-    let assert = run_with(&["start", "-r", "100"]);
+    let assert = run_with("start -r 100");
     assert.success();
 }
 
 #[test]
-fn starts_program_needs_reference_weight() {
-    let assert = run_with(&["start"]);
+fn starting_program_needs_reference_weight() {
+    let assert = run_with("start");
     assert
         .failure()
         .stderr(predicate::str::contains("REFERENCE_WEIGHT"));
 }
 
-fn run_with(args: &[&str]) -> assert_cmd::assert::Assert {
+fn run_with(args: &str) -> assert_cmd::assert::Assert {
     Command::cargo_bin(crate_name!())
         .unwrap()
-        .args(args)
+        .args(args.split_whitespace())
         .assert()
 }
