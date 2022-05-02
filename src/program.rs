5bbc1a336203ab2374d847d32600369594541921
Yogesh Dhamija <ydhamija96@gmail.com>
2022-05-02 08:31:42 -0500

    WIP: domain

--- src/program.rs
+++ src/program.rs
@@ -1,6 +1,6 @@
 use anyhow::{anyhow, Result};
+use std::time::Duration;
 
-#[allow(dead_code)]
 #[derive(Debug, PartialEq)]
 pub enum WeightScheme {
     BasedOnReference { multiplier: f64, offset: i64 },
@@ -9,30 +9,35 @@ pub enum WeightScheme {
     LinearBasedOnPrevious { amount_to_increase: u64 },
 }
 
-#[allow(dead_code)]
 impl WeightScheme {
+    /// notation options:
+    /// ```
+    /// parse("any")
+    /// parse("3.14r+12")
+    /// parse("3.14r-12")
+    /// parse("add10")
+    /// ```
     fn parse(notation: &str) -> Result<Self> {
         let error = "Invalid notation";
-        if notation == "any" {
-            return Ok(WeightScheme::Any);
+        return if notation == "any" {
+            Ok(WeightScheme::Any)
         } else if notation.contains('r') {
             // 3.14r+12
             let mut split = notation.split('r');
-            return Ok(WeightScheme::BasedOnReference {
+            Ok(WeightScheme::BasedOnReference {
                 multiplier: split.next().ok_or(anyhow!(error))?.parse()?,
                 offset: split.next().unwrap_or("0").parse().unwrap_or(0),
-            });
+            })
         } else {
             // add20
             let add_str = notation.split("add").skip(1).next().ok_or(anyhow!(error))?;
-            return Ok(WeightScheme::LinearBasedOnPrevious {
+            Ok(WeightScheme::LinearBasedOnPrevious {
                 amount_to_increase: add_str.parse()?,
-            });
-        }
+            })
+        };
     }
 }
 
-#[allow(dead_code)]
 #[derive(Debug, PartialEq)]
 pub enum Set {
     Amrap {
@@ -46,32 +51,47 @@ pub enum Set {
     Defined {
         reps: u64,
     },
+    Time {
+        duration: Duration,
+    },
 }
 
-#[allow(dead_code)]
 impl Set {
+    /// notation options:
+    /// ```
+    /// parse("8-12")
+    /// parse("3+")
+    /// parse("Any")
+    /// parse("10")
+    /// parse("5s")
+    /// ```
     fn parse(notation: &str) -> Result<Self> {
         let error = "Invalid notation";
-        if notation.contains('-') {
+        return if notation == "Any" {
+            Ok(Set::Any)
+        } else if notation.contains('-') {
             let mut split = notation.split('-');
-            return Ok(Set::Range {
+            Ok(Set::Range {
                 minimum_reps: split.next().ok_or(anyhow!(error))?.parse()?,
                 maximum_reps: split.next().ok_or(anyhow!(error))?.parse()?,
-            });
+            })
         } else if notation.contains('+') {
             let rep_string = notation.split('+').next().ok_or(anyhow!(error))?;
-            return Ok(Set::Amrap {
+            Ok(Set::Amrap {
                 minimum_reps: rep_string.parse()?,
-            });
+            })
+        } else if notation.contains('s') {
+            let seconds_str = notation.split('s').next().ok_or(anyhow!(error))?;
+            let duration = Duration::from_secs(seconds_str.parse()?);
+            Ok(Set::Time { duration })
         } else {
-            return Ok(Set::Defined {
+            Ok(Set::Defined {
                 reps: notation.parse()?,
-            });
-        }
+            })
+        };
     }
 }
 
-#[allow(dead_code)]
 #[derive(Debug, PartialEq)]
 pub struct Lift {
     name: String,
@@ -79,7 +99,6 @@ pub struct Lift {
     weight: WeightScheme,
 }
 
-#[allow(dead_code)]
 impl Lift {
     fn parse(notation: &str) -> Result<Self> {
         let error = "Cannot parse notation";
@@ -129,33 +148,64 @@ mod private {
     }
 }
 
-#[allow(dead_code)]
 #[derive(Debug)]
 pub struct Day {
     name: String,
     lifts: Vec<Lift>,
 }
 
-#[allow(dead_code)]
 #[derive(Debug)]
 pub struct Program {
     days: Vec<Day>,
 }
 
-#[allow(dead_code)]
 impl Program {
-    fn new() -> Self {
+    fn gzcl_4day() -> Self {
         Program {
-            days: vec![Day {
-                name: "Pull".to_string(),
-                lifts: vec![
-                    Lift::parse("Weighted Pullup -> 4x3,1x3+ @ 0.5r-30").unwrap(),
-                    Lift::parse("Weighted Pullup -> 4x3,1x3+ @ 0.5r-30").unwrap(),
-                    Lift::parse("Weighted Pullup -> 4x3,1x3+ @ 0.5r-30").unwrap(),
-                    Lift::parse("Weighted Pullup -> 4x3,1x3+ @ 0.5r-30").unwrap(),
-                    Lift::parse("Weighted Pullup -> 4x3,1x3+ @ 0.5r-30").unwrap(),
-                ],
-            }],
+            days: vec![
+                Day {
+                    name: "Pull".to_string(),
+                    lifts: vec![
+                        Lift::parse("Weighted Pullup -> 4x3,1x3+ @ 0.5r-30").unwrap(),
+                        Lift::parse("Pullup -> 3x7+").unwrap(),
+                        Lift::parse("Barbell Row -> 3x10 @ 0.65r").unwrap(),
+                        Lift::parse("Face Pull -> 2x15,1x15-25 @ add20").unwrap(),
+                        Lift::parse("Cable Curl -> 2x15,1x15-25 @ add20").unwrap(),
+                    ],
+                },
+                Day {
+                    name: "Push".to_string(),
+                    lifts: vec![
+                        Lift::parse("Bench press -> 4x3,1x3+ @ 1r").unwrap(),
+                        Lift::parse("Overhead press -> 3x10 @ 0.5r").unwrap(),
+                        Lift::parse("Incline bench press -> 3x10 @ 0.6r").unwrap(),
+                        Lift::parse("Pushup -> 3x15+").unwrap(),
+                        Lift::parse("Tricep Cable Pressdown -> 2x15,1x15-25 @ add20").unwrap(),
+                    ],
+                },
+                Day {
+                    name: "Legs".to_string(),
+                    lifts: vec![
+                        Lift::parse("Squat -> 4x3,1x3+ @ 1.35r").unwrap(),
+                        Lift::parse("Deadlift -> 3x8 @ 1.25r").unwrap(),
+                        Lift::parse("Romanian Deadlift -> 3x10 @ 0.675r").unwrap(),
+                        Lift::parse("Leg press -> 2x15,1x15-25 @ add30").unwrap(),
+                        Lift::parse("Standing dumbbell calf raise -> 2x15,1x15-25 @ add20")
+                            .unwrap(),
+                    ],
+                },
+                Day {
+                    name: "Core".to_string(),
+                    lifts: vec![
+                        Lift::parse("Plank -> 1x30s @ any").unwrap(),
+                        Lift::parse("Ab Rollout -> 3xAny").unwrap(),
+                        Lift::parse("Cable Core Press -> 3xAny @ any").unwrap(),
+                        Lift::parse("Bent-knee reverse hyperextension -> 3xAny @ any").unwrap(),
+                        Lift::parse("Knee raises -> 3xAny").unwrap(),
+                        Lift::parse("Leg extensions -> 3xAny @ any").unwrap(),
+                    ],
+                },
+            ],
         }
     }
 }
@@ -163,14 +213,19 @@ impl Program {
 #[cfg(test)]
 mod tests {
     use crate::program::*;
+    use std::time::Duration;
+
+    #[test]
+    fn can_create_program() {
+        Program::gzcl_4day();
+    }
 
     #[test]
     fn can_create_lift_schemes() {
         assert!(Lift::parse(":(").is_err());
-        let a = Lift::parse("Pullups -> 2x5,1x5-7,1x5+ @ 0.2r").unwrap();
-        println!("{a:#?}");
-        assert!(
-            a == Lift {
+        assert_eq!(
+            Lift::parse("Pullups -> 2x5,1x5-7,1x5+ @ 0.2r").unwrap(),
+            Lift {
                 name: "Pullups".to_string(),
                 sets: vec![
                     Set::parse("5").unwrap(),
@@ -186,47 +241,54 @@ mod tests {
     #[test]
     fn can_create_rep_schemes() {
         assert!(Set::parse(":(").is_err());
-        assert!(
-            Set::parse("2-3").unwrap()
-                == Set::Range {
-                    maximum_reps: 3,
-                    minimum_reps: 2
-                }
+        assert_eq!(
+            Set::parse("2-3").unwrap(),
+            Set::Range {
+                maximum_reps: 3,
+                minimum_reps: 2
+            }
         );
-        assert!(Set::parse("3+").unwrap() == Set::Amrap { minimum_reps: 3 });
-        assert!(Set::parse("3").unwrap() == Set::Defined { reps: 3 });
+        assert_eq!(Set::parse("3+").unwrap(), Set::Amrap { minimum_reps: 3 });
+        assert_eq!(Set::parse("3").unwrap(), Set::Defined { reps: 3 });
+        assert_eq!(Set::parse("Any").unwrap(), Set::Any);
+        assert_eq!(
+            Set::parse("2s").unwrap(),
+            Set::Time {
+                duration: Duration::new(2, 0)
+            }
+        );
     }
 
     #[test]
     fn can_create_weight_schemes() {
         assert!(WeightScheme::parse(":(").is_err());
-        assert!(
-            WeightScheme::parse("3.14r-12").unwrap()
-                == WeightScheme::BasedOnReference {
-                    multiplier: 3.14,
-                    offset: -12
-                }
+        assert_eq!(
+            WeightScheme::parse("3.14r-12").unwrap(),
+            WeightScheme::BasedOnReference {
+                multiplier: 3.14,
+                offset: -12
+            }
         );
-        assert!(
-            WeightScheme::parse("3.14r+12").unwrap()
-                == WeightScheme::BasedOnReference {
-                    multiplier: 3.14,
-                    offset: 12
-                }
+        assert_eq!(
+            WeightScheme::parse("3.14r+12").unwrap(),
+            WeightScheme::BasedOnReference {
+                multiplier: 3.14,
+                offset: 12
+            }
         );
-        assert!(
-            WeightScheme::parse("3.14r").unwrap()
-                == WeightScheme::BasedOnReference {
-                    multiplier: 3.14,
-                    offset: 0
-                }
+        assert_eq!(
+            WeightScheme::parse("3.14r").unwrap(),
+            WeightScheme::BasedOnReference {
+                multiplier: 3.14,
+                offset: 0
+            }
         );
-        assert!(WeightScheme::parse("any").unwrap() == WeightScheme::Any);
-        assert!(
-            WeightScheme::parse("add20").unwrap()
-                == WeightScheme::LinearBasedOnPrevious {
-                    amount_to_increase: 20
-                }
+        assert_eq!(WeightScheme::parse("any").unwrap(), WeightScheme::Any);
+        assert_eq!(
+            WeightScheme::parse("add20").unwrap(),
+            WeightScheme::LinearBasedOnPrevious {
+                amount_to_increase: 20
+            }
         );
     }
 }
