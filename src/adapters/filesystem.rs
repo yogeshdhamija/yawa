d3969c083b65c6e6549ab9cf761065fc028e9967
Yogesh Dhamija <ydhamija96@gmail.com>
2022-05-03 13:33:32 -0500

    chore: refactor: saved data is more readable

--- src/adapters/filesystem.rs
+++ src/adapters/filesystem.rs
@@ -1,10 +1,9 @@
 use crate::programs::Program;
 use crate::services::ports::PersistenceAdapter;
 use anyhow::Result;
-use serde::{Deserialize, Serialize};
 use std::fs::create_dir_all;
 use std::fs::File;
-use std::io::BufReader;
+use std::io::Read;
 use std::io::Write;
 
 pub struct FileSystem {}
@@ -13,26 +12,18 @@ pub fn new() -> FileSystem {
     FileSystem {}
 }
 
-#[derive(Serialize, Deserialize)]
-struct Saveable {
-    program: String,
-}
-
 impl PersistenceAdapter for FileSystem {
     fn persist(&self, program: &Program) -> Result<()> {
         create_dir_all("/tmp/yawa")?;
         let mut file = File::create("/tmp/yawa/saved.json")?;
         let program_string = format!("{program}");
-        let json = serde_json::to_string_pretty(&Saveable {
-            program: program_string,
-        })?;
-        write!(file, "{json}")?;
+        write!(file, "{program_string}")?;
         Ok(())
     }
     fn summon(&self) -> Result<Program> {
-        let file = File::open("/tmp/yawa/saved.json")?;
-        let reader = BufReader::new(file);
-        let saved: Saveable = serde_json::from_reader(reader)?;
-        Ok(Program::parse(&saved.program)?)
+        let mut file = File::open("/tmp/yawa/saved.json")?;
+        let mut program_string = String::new();
+        file.read_to_string(&mut program_string)?;
+        Ok(Program::parse(&program_string)?)
     }
 }
