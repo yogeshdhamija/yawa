8078aa1e3f4a2af9fdc68deb251e80b3971eeee6
Yogesh Dhamija <ydhamija96@gmail.com>
2024-12-28 18:58:44 -0600

    chore: remove unhelpful docs

--- src/domain/mod.rs
+++ src/domain/mod.rs
@@ -1,8 +1,5 @@
-/// Useful primitives like Sets/Weights
 pub mod lifting;
-/// Useful primitives: the program
 pub mod programs;
-/// Useful primitives: user actions
 pub mod user_input;
 pub mod weight_scheme;
 pub mod set;
